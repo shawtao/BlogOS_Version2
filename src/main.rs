d8f93cd60529b5903971caccd1fc0b630a1314fe
shawtao <1203405394@qq.com>
2021-06-23 23:46:58 +0800

    fininsh !println

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,14 @@ static HELLO: &[u8] = b"Hello World!";
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    //vga_buffer::print_something();
+    println!("Hello World{}", "!");
+    panic!("Some panic message");
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,5 +1,29 @@
+use volatile::Volatile;
+use core::fmt::{self, Write};
+use lazy_static::lazy_static;
+use spin::Mutex;
+
+
 const BUFFER_HEIGHT: usize = 25;
 const BUFFER_WIDTH: usize = 80;
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($scrate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
+}
+
 
 #[allow(dead_code)] //禁用对没使用变量的警告
 #[derive(Debug, Clone, Copy, PartialEq, Eq)] //生成这几个trait，让枚举类型遵循复制语义
@@ -42,7 +66,7 @@ struct ScreenChar {
 
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],  //Volatile确保我们不会通过普通的写入操作，意外向它写入数据
 }
 
 pub struct Writer {
@@ -62,10 +86,10 @@ impl Writer {
                 let row = BUFFER_HEIGHT - 1;
                 let col = self.column_position;
 
-                self.buffer.chars[row][col] = ScreenChar {
+                self.buffer.chars[row][col].write(ScreenChar{
                     ascii_character : byte,
                     color_code : self.color_code,
-                };
+                });
                 self.column_position += 1;
             }
         }
@@ -80,10 +104,44 @@ impl Writer {
         }
     }
 
-    fn newline(&mut self) {}
+    fn newline(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character : b' ',
+            color_code : self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH - 1 {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
 }
 
-pub fn print_something() {
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
+lazy_static! {  //使用laze_static! 和 spin::Mutex声明一个全局可变的Writer变量
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        column_position : 0,
+        color_code : ColorCode::new(Color::Blue, Color::Pink),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer)}
+    });
+}
+
+/*pub fn print_something() {
     let mut writer = Writer {
         column_position : 0,
         color_code : ColorCode::new(Color::Green, Color::Red),
@@ -93,4 +151,6 @@ pub fn print_something() {
     writer.write_byte(b'H');
     writer.write_string("ello ");
     writer.write_string("world!");
-}
+    write!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
+} */
+
